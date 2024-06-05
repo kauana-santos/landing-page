fa5430b5b8ab8125476eda425b0a5084ca46586f
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2024-06-05 17:50:57 -0300

    código

--- landing-page/script.js
+++ landing-page/script.js
@@ -10,3 +10,44 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+$(function(){
+
+    function autualizar(){
+        let prodPrice =  parseFloat($(".slick-center").data("price")).toLocaleString("pt-BR", {style: "currency", currency:"BRL", minimumFractionDigits: 2});
+        
+        $("#prod-name").text( $(".slick-center").data("name") ); 
+        $("#prod-price").text( prodPrice);
+    }
+
+    $(".produtos").on('init', function(){
+        
+        autualizar();
+     })
+
+    $(".produtos").slick({
+        slidesToShow: 3,
+        infinite: true,
+        slidesToScroll: 1,
+        centerMode: true,
+        nextArrow:$("#arrow-next"),
+        prevArrow:$("#arrow-prev"),
+        responsive: [
+            {
+              breakpoint: 640,
+              settings: {
+                slidesToShow: 1,
+                
+              }
+            }
+           
+            
+          ]
+    });
+
+    $(".produtos").on('afterChange', function(){
+        
+       autualizar();
+    })
+})
